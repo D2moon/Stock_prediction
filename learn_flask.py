6eb6dea06ce9bd6fe15bb1b9b6568f5a6694a797
weibolu <1970353515@qq.com>
2021-05-10 10:44:09 +0800

    Flask基础学习

--- learn_flask.py
+++ learn_flask.py
@@ -5,6 +5,9 @@ import click
 from flask import Flask, render_template
 from flask import request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user
+from flask_login import login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
@@ -12,11 +15,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
@@ -34,6 +47,8 @@ def inject_user():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form.get('tittle')
         year = request.form.get('year')
         if not title or not year or len(year) > 4 or len(title) > 60:
@@ -51,6 +66,52 @@ def index():
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye')
+    return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
 
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
@@ -73,6 +134,7 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -88,7 +150,7 @@ def page_not_found(e):
 
 @app.cli.command()
 def forge():
-    name = 'Grey Li'
+    name = 'Wbl'
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -109,3 +171,50 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_watchlist.py
@@ -0,0 +1,123 @@
+import unittest
+
+from learn_flask import app, db, Movie, User
+
+
+class WatchlistTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        app.config.update(
+            TESTING=True,
+            SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
+        )
+        db.create_all()
+        user = User(name='Test', username='test')
+        user.set_password('123')
+        movie = Movie(title='Test Movie Title', year='2019')
+        db.session.add_all([user, movie])
+        db.session.commit()
+
+        self.client = app.test_client()
+        self.runner = app.test_cli_runner()
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+
+    def test_app_exist(self):
+        self.assertIsNotNone(app)
+
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['TESTING'])
+
+    def test_404_page(self):
+        response = self.client.get('/nothing')
+        data = response.get_data(as_text=True)
+        self.assertIn('Page Not Found - 404', data)
+        self.assertIn('Go Back', data)
+        self.assertEqual(response.status_code, 404)
+
+    def test_index_page(self):
+        response = self.client.get('/')
+        data = response.get_data(as_text=True)
+        self.assertIn('Test\'s Watchlist', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertEqual(response.status_code, 200)
+
+    def login(self):
+        self.client.post('/login', data=dict(
+            username='test',
+            password='123'
+        ), follow_redirects=True)
+
+    def test_create_item(self):
+        self.login()
+
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item created.', data)
+        self.assertIn('New Movie', data)
+
+        response = self.client.post('/', data=dict(
+            title='/',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+    def test_update_item(self):
+        self.login()
+
+        response = self.client.get('/movie/edit/1')
+        data = response.get_data(as_text=True)
+        self.assertIn('Edit item', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertIn('2019', data)
+
+        response = self.client.post('movie/edit/1', data=dict(
+            title='New Movie Edited',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item updated.', data)
+        self.assertIn('New Movie Edited', data)
+
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertIn('Invalid input.', data)
+
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='New Movie Edited Again',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertNotIn('New Movie Edited Again', data)
+        self.assertIn('Invalid input.', data)
+
+    def test_delete_item(self):
+        self.login()
+
+        response = self.client.post('/movie/delete/1', follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item deleted.', data)
+        self.assertNotIn('Test Movie Title', data)
+
+
+
+
